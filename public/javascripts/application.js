12b3315ff8d97a598d40cd76208c1004dc4e6fc2
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-02-06 11:09:57 +0100

    Change socket url.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function () {
-  var socket = io.connect("http://localhost");
+  var socket = io.connect("http://porchetta.herokuapp.com/");
 
   socket.on("status", function(data) {
     console.log(data.status);
