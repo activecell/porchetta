55726313db4547ade48564fad6708e47052b3770
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-06-09 13:18:52 +0600

    Finished test suite for sync event

--- test/index-test.js
+++ test/index-test.js
@@ -3,24 +3,44 @@ require('..');
 var expect  = require('chai').expect;
 var _       = require('underscore');
 var connect = require('./support/connect');
-var bertCooper, rogerSterling, peteCampbell;
+var timeout = function(cb) { return _.delay(cb, 20); };
+var bertCooper, rogerSterling, peteCambell;
 
 describe('Porchetta', function(){
   beforeEach(function(done) {
-    var next = _.after(3, done);
+    var next = _.after(2, function() {
+      bertCooper.emit('room', 1);
+      rogerSterling.emit('room', 1);
+      peteCambell.emit('room', 2);
+      timeout(done);
+    });
 
     bertCooper    = connect(next);
     rogerSterling = connect(next);
-    peteCampbell  = connect(next);
+    peteCambell   = connect(next);
+  });
+
+  it('has `sync` event', function(done) {
+    bertCooper.emit('sync', { name: 'vendors', json: { id: 1, name: 'test' } });
+
+    rogerSterling.on('sync', function(data) {
+      expect(data.name).equal('vendors');
+      expect(data.json).exists;
+      timeout(done);
+    });
+
+    bertCooper.on('sync', function() {
+      done('error: it does not return event back');
+    });
+
+    peteCambell.on('sync', function() {
+      done('error: it broadcasts only in room with companyId');
+    });
   });
 
   afterEach(function() {
     bertCooper.disconnect();
     rogerSterling.disconnect();
-    peteCampbell.disconnect();
-  });
-
-  it('has `sync` event', function() {
-
+    peteCambell.disconnect();
   });
 });
