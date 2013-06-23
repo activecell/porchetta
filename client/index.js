ce69d92cd3b4f14d9b54465c01cade44c80bd839
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-06-23 15:27:45 +0600

    Mockup for Porchetta client

---
+++ client/index.js
@@ -0,0 +1,32 @@
+
+/**
+ * Local variables.
+ */
+
+var io = require('socket.io') ;
+
+/**
+ * Expose `Porchetta`.
+ */
+
+module.exports = Porchetta;
+
+/**
+ * Create `Porchetta` instance.
+ *
+ * @param {Object} options
+ */
+
+function Porchetta(options) {
+  // body...
+}
+
+/**
+ * Observe `collection`.
+ *
+ * @param {Backbone.Collection} collection
+ */
+
+Porchetta.prototype.add = function(collection) {
+  // body...
+};
