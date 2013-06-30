fbf5578af56b55c8ab907924c97f9ec80291d5bf
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-06-30 16:02:53 +0600

    Added Emitter to porchetta API

--- client/index.js
+++ client/index.js
@@ -2,17 +2,15 @@
  * Local variables.
  */
 
-var io = require('socket.io');
-var active = false;
-var collections = {};
-var socket;
+var io      = require('socket.io');
+var bind    = require('bind');
+var Emitter = require('emitter');
 
 /**
- * Expose `connect` and `add` methods.
+ * Expose `Porchetta`.
  */
 
-exports.connect = connect;
-exports.add     = add;
+module.exports = Porchetta;
 
 /**
  * Connect to porchetta server by `url`
@@ -20,22 +18,31 @@ exports.add     = add;
  *
  * Example:
  *
- *   porchetta.connect(http://localhost:4000, app.company.id);
+ *   new Porchetta(http://localhost:4000, app.company.id, { 'force new connection': true });
  *
  * @param {String} url
  * @param {String} room
+ * @param {Object} options - socket.io-client options
+ * @api public
  */
 
-function connect(url, room) {
-  socket = io.connect(url);
-  socket.on('viewers', viewers);
-  socket.on('sync', sync);
+function Porchetta(url, room, options) {
+  this.active = false;
+  this.colls  = {};
+  this.socket = io.connect(url, options);
+
+  this.subscribe('viewers', this.onviewers);
+  this.subscribe('sync', this.onsync);
 
   // join room
-  socket.on('connect', function() {
-    socket.emit('room', room);
+  this.subscribe('connect', function() {
+    this.socket.emit('room', room);
+    this.emit('connect');
   });
 }
+
+// Add event-emitter options
+Emitter(Porchetta.prototype);
 
 /**
  * Observe `collection`.
@@ -43,48 +50,32 @@ function connect(url, room) {
  *
  * Example:
  *
- *   porchetta.add(app.vendors);
- *   porchetta.add(app.accounts);
+ *   porchetta
+ *     .add(app.vendors);
+ *     .add(app.accounts);
  *
  * @param {Backbone.Collection} collection
+ * @api public
  */
 
-function add(collection) {
+Porchetta.prototype.add = function(collection) {
   var name = collection.name;
   if (!name || collections[name]) throw new TypeError('Collection should have unique name attribute');
 
-  collections[name] = collection;
-  collection.on('add', emit('add', name));
-  collection.on('change', emit('change', name));
-  collection.on('remove', emit('remove', name));
-}
+  this.colls[name] = collection;
+  collection.on('add', this.handleEvent('add', name), this);
+  collection.on('change', this.handleEvent('change', name), this);
+  collection.on('remove', this.handleEvent('remove', name), this);
 
-/**
- * Emit sync event on every collection change.
- * Helps to manage `add`, `change`, and `remove` events.
- */
-
-function emit(event, name) {
-  return function(model, collection, options) {
-    if (!active) return; // return if only one viewers
-    if (!options) options = collection; // for change event
-    if (options.socketId) return; // prevent updates after sync
-
-    socket.emit('sync', {
-      collection: name,
-      event: event,
-      socketId: socket.socket.sessionid,
-      json: model.toJSON()
-    });
-  };
-}
+  return this;
+};
 
 /**
  * Handles `sync` event to update collection based on received data.
  */
 
-function sync(data) {
-  var model, collection = collections[data.collection];
+Porchetta.prototype.onsync = function(data) {
+  var collection = this.colls[data.collection], model;
 
   switch (data.event) {
     case 'add':
@@ -101,16 +92,43 @@ function sync(data) {
       if (model) collection.remove(model, { socketId: data.socketId });
       break;
   }
-}
+
+  this.emit(data.collection + ':' + data.event, data.json);
+};
 
 /**
  * Handles `viewers` event to change active flag.
  */
 
-function viewers(data) {
-  active = data.length > 1;
-  if (active)
-    console.log('Porchetta is active for ' + data.length + ' viewers');
-  else
-    console.log('Porchetta is inactive');
-}
+Porchetta.prototype.onviewers = function(data) {
+  this.active = data.length > 1;
+  this.emit('viewers', data);
+};
+
+/**
+ * Helper, that emits sync event on every collection change.
+ * Helps to manage `add`, `change`, and `remove` events.
+ */
+
+Porchetta.prototype.handleEvent = function(event, name) {
+  return function(model, collection, options) {
+    if (!this.active) return; // return if only one viewer
+    if (!options) options = collection; // for change event
+    if (options.socketId) return; // prevent updates after sync
+
+    this.socket.emit('sync', {
+      collection: name,
+      event: event,
+      socketId: this.socket.socket.sessionid,
+      json: model.toJSON()
+    });
+  };
+};
+
+/**
+ * Subscribe on socket.io events and bind callback to current instance.
+ */
+
+Porchetta.prototype.subscribe = function(event, cb) {
+  this.socket.on(event, bind(this, cb));
+};
